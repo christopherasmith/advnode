adf06fee485899c979e1f590a8125222f4fa346d
christopherasmith <chrisasmith@yahoo.com>
2020-07-25 19:09:14 -0400

    Added delete to server3.js

--- server3.js
+++ server3.js
@@ -34,9 +34,27 @@ mongoClient.connect('mongodb://localhost:27017/todo', (err, client)=>{
             console.log(JSON.stringify(result.ops, undefined, 2));
           }
         )
-*/
 
-        db.collection('users').find().toArray().then(
+        db.collection('todos').deleteMany({completed: false}).then(
+          (result)=>{console.log(`Result: ${result.result.ok?"OK":"Failed"}, Count: ${result.result.n}`)},
+          (err)=>{console.log(`error deleting docs: ${err}`)}
+        )
+
+        db.collection('todos').deleteOne({text: "Eat breakfast"}).then(
+          (result)=>{console.log(`Result: ${result.result.ok?"OK":"Failed"}, Count: ${result.result.n}`)},
+          (err)=>{console.log(`error deleting docs: ${err}`)}
+        )
+        */
+
+        db.collection('todos').findOneAndDelete({text: "Eat breakfast"}).then(
+          (result)=>{
+            console.log(`Value: ${JSON.stringify(result.value)}`);
+            console.log(`Result: ${result.ok?"OK":"Failed"}, Count: ${result.lastErrorObject.n}`)
+          },
+          (err)=>{console.log(`error deleting docs: ${err}`)}
+        )
+
+        db.collection('todos').find().toArray().then(
           (docs)=>{ console.log(docs);},
           (err)=>{console.log(`error finding docs: ${err}`)}
         );
